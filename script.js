b3fa45fbf3064d6bf1c9d684232a7fab1b83ebd7
Lohit Kumar <lohitkumarlohitkumar02@gmail.com>
2025-08-10 22:09:06 +0530

    social and projects section

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+const projectBtn = document.querySelector(".projects");
+const projectsSection = document.querySelector(".projects_section");
+
+projectBtn.addEventListener("click", () => {
+  projectsSection.scrollIntoView({ 
+    behavior: "smooth",
+    block: "start"});
+});
+
 gsap.from("header h1", {
-    width:0,
-    duration: 8
-})
+  width: 0,
+  duration: 8,
+});
